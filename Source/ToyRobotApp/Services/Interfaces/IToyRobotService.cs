e380a857276f6195ca9402827c6671ac8747d8b3
joshua anderson <j5anderson@hotmail.co.uk>
2022-09-22 15:17:22 +0100

    Added  more detail to some comments

--- Source/ToyRobotApp/Services/Interfaces/IToyRobotService.cs
+++ Source/ToyRobotApp/Services/Interfaces/IToyRobotService.cs
@@ -14,10 +14,11 @@ namespace ToyRobotApp.Services.Interfaces
     {
         /// <summary>
         /// This function carries out all the commands given by the user on the robot. All the commands are executed, as long as none result in the robot falling off the table.
+        /// All commands are in string format and parsed accordinly in their respective functions.
         /// </summary>
         /// <param name="robot">The robot that will be moved based on the given commands. Its state is stored in this object to ensure it stays within the bounds.</param>
         /// <param name="commandList">The given list of commands the user submitted.</param>
-        /// <returns></returns>
+        /// <returns>NewPlacementResult containing the robots new placement on the board</returns>
         NewPlacementResult ProcessCommands(Robot robot, List<string> commandList);
 
     }
